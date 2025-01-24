946cd94be4a8c9673f250df0045ed571ed644c0f
KYSSQLF <kayassle@hotmail.com>
2025-01-24 15:10:03 +0000

    maj

---
+++ app.js
@@ -0,0 +1,61 @@
+
+let x = 20;
+
+let y = 3;
+
+let a = x % y;
+
+console.log(a);
+console.log(x===y);
+
+//if ... else ...
+let moyenne = 12;
+    
+    if(moyenne<12) {
+    console.log("dommage tu feras mieux la prochaine fois");
+    }
+    else{
+        console.log("bravo");
+    }
+
+let note = 15;
+
+    if(note<=5)
+    {
+    console.log("premier strike + 1 semaine de cour de soutient");
+    }
+
+    else if(note > 5 && note <= 10 )
+    {
+    console.log("tu as gagner des heures de rattrapage tous les soirs")
+    }
+
+    else if(note > 10 && note <=15)
+    {
+    console.log("2H de soutient");    
+    }
+
+    else if(note > 10 && note <=12)
+    {
+    console.log("Peu mieux faire");
+    }
+
+    else{
+        console.log("continue comme cela");
+    }
+
+    let PommesAvaler = 0;
+    let totalPommes = 5;
+
+    for(let i = 0;i< 5; i++) {
+        console.log(i);
+    }
+    
+
+
+
+
+
+    
+
+    
